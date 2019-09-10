6ab6b82d316e1faae5671d108204572c4cd4938b
Guy Margalit <guymguym@gmail.com>
2019-09-10 14:01:33 +0300

    noobaa v1.1.1

--- Formula/noobaa.rb
+++ Formula/noobaa.rb
@@ -2,14 +2,14 @@ class Noobaa < Formula
   desc "CLI for managing NooBaa S3 service on Kubernetes/Openshift"
   homepage "https://github.com/noobaa/noobaa-operator"
   url "https://github.com/noobaa/noobaa-operator.git",
-      :tag      => "v1.1.0",
-      :revision => "2e2d167bbe208f3d30773b78ffcec566d8ad7bc5"
+      :tag      => "v1.1.1",
+      :revision => "9d7746b0d73baf39618de5fdc3465619cc2ebebe"
   head "https://github.com/noobaa/noobaa-operator.git"
 
   bottle do
     root_url "https://github.com/noobaa/homebrew-noobaa/releases/download/v1.1.0"
     cellar :any_skip_relocation
-    sha256 "25409324132b49dc7d0eeec89722cbbcca89af9adc846fe26e764d73fd49d2ea" => :mojave
+    sha256 "e948e81a6d52c03b1fddde7e82d2cede89d702d8b3953a19b5335d4d352607bc" => :mojave
   end
   
   depends_on "go" => [:build, :test]
@@ -21,6 +21,8 @@ class Noobaa < Formula
     src = buildpath/"src/github.com/noobaa/noobaa-operator"
     src.install buildpath.children
     src.cd do
+      mkdir_p "./build/_output/bundle"
+      File.write "./build/_output/bundle/empty.go", "package bundle"
       system "go", "mod", "vendor"
       system "go", "generate"
       system "go", "build"
@@ -30,7 +32,7 @@ class Noobaa < Formula
 
   test do
     output = `#{bin}/noobaa version 2>&1`
-    pos = output.index "CLI version: 1.1.0"
+    pos = output.index "CLI version: 1.1.1"
     raise "Version check failed" if pos.nil?
 
     puts "Success"
