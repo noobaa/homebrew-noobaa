4ba8ce7ce8e8543d095bab03a3fe8cba9e513425
Guy Margalit <guymguym@gmail.com>
2019-10-03 18:52:56 +0300

    v2.0.1

--- Formula/noobaa.rb
+++ Formula/noobaa.rb
@@ -2,14 +2,14 @@ class Noobaa < Formula
   desc "CLI for managing NooBaa S3 service on Kubernetes/Openshift"
   homepage "https://github.com/noobaa/noobaa-operator"
   url "https://github.com/noobaa/noobaa-operator.git",
-      :tag      => "v2.0.0",
-      :revision => "ce4e8dacb8d61260c44630d27a2094e0c3419c74"
+      :tag      => "v2.0.1",
+      :revision => "1d8de9d59634788d7581bd6725dcb97fb51664e4"
   head "https://github.com/noobaa/noobaa-operator.git"
 
   bottle do
-    root_url "https://github.com/noobaa/homebrew-noobaa/releases/download/v2.0.0"
+    root_url "https://github.com/noobaa/homebrew-noobaa/releases/download/v2.0.1"
     cellar :any_skip_relocation
-    sha256 "19608b0a973ac3a037c4da52992a28506b7f42311e7b00efa1fbf072c566c06d" => :mojave
+    sha256 "e150f97eb44b1804c415d550e6a101fb1ef6dd413ae03f9e67ecd153cb269881" => :mojave
   end
   
   depends_on "go" => [:build, :test]
@@ -33,7 +33,7 @@ class Noobaa < Formula
 
   test do
     output = `#{bin}/noobaa version 2>&1`
-    pos = output.index "CLI version: 2.0.0"
+    pos = output.index "CLI version: 2.0.1"
     raise "Version check failed" if pos.nil?
 
     puts "Success"
