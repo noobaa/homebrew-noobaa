0bbf8393527c893b2f57adf6be9d4f1f18eaa7d1
Guy Margalit <guymguym@gmail.com>
2019-10-25 02:14:54 +0300

    v2.0.4

--- Formula/noobaa.rb
+++ Formula/noobaa.rb
@@ -2,14 +2,14 @@ class Noobaa < Formula
   desc "CLI for managing NooBaa S3 service on Kubernetes/Openshift"
   homepage "https://github.com/noobaa/noobaa-operator"
   url "https://github.com/noobaa/noobaa-operator.git",
-      :tag      => "v2.0.3",
-      :revision => "0a9b63722cbbc320ac31b7856821ed744ca1f1a6"
+      :tag      => "v2.0.4",
+      :revision => "b489207f6543cf6fb7afbff4ecb5b93de98a890e"
   head "https://github.com/noobaa/noobaa-operator.git"
 
   bottle do
-    root_url "https://github.com/noobaa/homebrew-noobaa/releases/download/v2.0.3"
+    root_url "https://github.com/noobaa/homebrew-noobaa/releases/download/v2.0.4"
     cellar :any_skip_relocation
-    sha256 "da4af431de7eca1ea70ef7e3b5965770053811355843c6a04d83104100f26c02" => :catalina
+    sha256 "0993ac7551edd47a674efdefac6bf33d84dd6a009274d016e989db0316a72c08" => :catalina
   end
   
   depends_on "go" => [:build, :test]
@@ -31,7 +31,7 @@ class Noobaa < Formula
 
   test do
     output = `#{bin}/noobaa version 2>&1`
-    pos = output.index "CLI version: 2.0.3"
+    pos = output.index "CLI version: 2.0.4"
     raise "Version check failed" if pos.nil?
 
     puts "Success"
--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ all: bottle
 .PHONY: all
 
 bottle:
+	brew uninstall noobaa
 	brew install --build-bottle noobaa
 	brew bottle noobaa
 	brew postinstall noobaa
