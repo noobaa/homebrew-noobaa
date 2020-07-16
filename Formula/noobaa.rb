6b2f9ce376897585e70f9e369315c703845bad4b
Danny Zaken <dannyzaken@gmail.com>
2020-07-16 12:28:31 +0300

    noobaa v2.3.0

--- Formula/noobaa.rb
+++ Formula/noobaa.rb
@@ -2,14 +2,14 @@ class Noobaa < Formula
   desc "CLI for managing NooBaa S3 service on Kubernetes/Openshift"
   homepage "https://github.com/noobaa/noobaa-operator"
   url "https://github.com/noobaa/noobaa-operator.git",
-      :tag      => "v2.2.0",
-      :revision => "c00a7d18f2ad8dddd1b8daf842093a54cb3456cd"
+      :tag      => "v2.3.0",
+      :revision => "416d6dedfb670545b345caa4083ef9ba07c2cf9a"
   head "https://github.com/noobaa/noobaa-operator.git"
 
   bottle do
-    root_url "https://github.com/noobaa/homebrew-noobaa/releases/download/v2.2.0"
+    root_url "https://homebrew.bintray.com/bottles-noobaa"
     cellar :any_skip_relocation
-    sha256 "88505225cfd36257f98840037d3f96d073fab7838c90349a0663e9846aad3ffc" => :mojave
+    sha256 "dfd07a1c3f2bc3b0b1f1b7d2f2f758914e51060986f8de5f7d2a5e82fbabef59" => :mojave
   end
   
   depends_on "go" => [:build, :test]
@@ -31,7 +31,7 @@ class Noobaa < Formula
 
   test do
     output = `#{bin}/noobaa version 2>&1`
-    pos = output.index "CLI version: 2.2.0"
+    pos = output.index "CLI version: 2.3.0"
     raise "Version check failed" if pos.nil?
 
     puts "Success"
