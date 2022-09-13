09a8fec4c01a13c5bcb9c546551e49ab9e351a9d
Danny Zaken <dannyzaken@gmail.com>
2022-09-13 20:28:25 +0300

    Update noobaa.rb

--- Formula/noobaa.rb
+++ Formula/noobaa.rb
@@ -2,14 +2,10 @@ class Noobaa < Formula
   desc "CLI for managing NooBaa S3 service on Kubernetes/Openshift"
   homepage "https://github.com/noobaa/noobaa-operator"
   url "https://github.com/noobaa/noobaa-operator.git",
-      :tag      => "v5.10.1",
-      :revision => "6f571fd49769063bb075a18c4302e9433ef8e421"
+      :tag      => "v5.11.0",
+      :revision => "3e55ae59d4e44677f6694efe757ff6e3d70a7dfd"
   head "https://github.com/noobaa/noobaa-operator.git"
 
-  bottle do
-    root_url "https://github.com/noobaa/noobaa-operator/releases/download/v5.10.1"
-    # sha256 cellar: :any_skip_relocation, monterey: "b163cf7bdf172dee76c891251c7b35e6dbe3277e200fda3b3b84c2a68e270031"
-  end
   depends_on "go" => [:build, :test]
 
   def install
