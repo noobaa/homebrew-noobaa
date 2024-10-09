a429c7531cbbc47a34199310dddeac871d563444
Guy Margalit <guymguym@gmail.com>
2024-10-09 23:40:28 +0300

    Update noobaa.rb to 5.16.2

--- Formula/noobaa.rb
+++ Formula/noobaa.rb
@@ -2,8 +2,8 @@ class Noobaa < Formula
   desc "CLI for managing NooBaa S3 service on Kubernetes/Openshift"
   homepage "https://github.com/noobaa/noobaa-operator"
   url "https://github.com/noobaa/noobaa-operator.git",
-      :tag      => "v5.14.2",
-      :revision => "e02239cd3d5c7b4b2fc0d5b5b1e76efb363ef5a6"
+      :tag      => "v5.16.2",
+      :revision => "260aebafe3e434cff8972dde3cf64ab5193f06dc"
   head "https://github.com/noobaa/noobaa-operator.git"
 
   depends_on "go" => [:build, :test]
@@ -26,7 +26,7 @@ class Noobaa < Formula
 
   test do
     output = `#{bin}/noobaa version 2>&1`
-    pos = output.index "CLI version: 5.12.4"
+    pos = output.index "CLI version: 5.16.2"
     raise "Version check failed" if pos.nil?
 
     puts "Success"
